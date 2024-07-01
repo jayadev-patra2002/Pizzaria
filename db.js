737b7f198270b4ed4131f3d1c7ffe231ba62aab5
jayadev-patra2002 <129044059+jayadev-patra2002@users.noreply.github.com>
2024-07-01 17:23:37 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://jayadev-2002:Chandu%40123@atlascluster.tiotyjw.mongodb.net/MERN_resturant');
+  await mongoose.connect('mongodb+srv://jayadev-2002:Chandu@123@atlascluster.tiotyjw.mongodb.net/MERN_resturant?retryWrites=true&w=majority&appName=AtlasCluster');
    console.log('database connect....')
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled)
 }
